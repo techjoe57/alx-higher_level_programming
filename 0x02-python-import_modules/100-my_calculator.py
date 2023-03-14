7114fe0773ea893f445300a923849ddb7e31b9e5
Techjoe-57 <117743341+Techjoe-57@users.noreply.github.com>
2023-03-14 08:41:20 +0000

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,45 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
+
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
     from calculator_1 import add, sub, mul, div
-    count = len(argv)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
-    if count != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        op = argv[2]
-
-        def not_found():
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-
-            def my_add():
-                total = add(num1, num2)
-                print("{:d} + {:d} = {:d}".format(num1, num2, total))
-                return total
-
-            def my_sub():
-                total = sub(num1, num2)
-                print("{:d} - {:d} = {:d}".format(num1, num2, total))
-                return total
-
-            def my_mul():
-                total = mul(num1, num2)
-                print("{:d} * {:d} = {:d}".format(num1, num2, total))
-                return total
-
-            def my_div():
-                total = div(num1, num2)
-                print("{:d} / {:d} = {:d}".format(num1, num2, total))
-                return total
-
-            options = {
-                "+": my_add,
-                "-": my_sub,
-                "*": my_mul,
-                "/": my_div
-            }
-            options.get(op, not_found)()
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))

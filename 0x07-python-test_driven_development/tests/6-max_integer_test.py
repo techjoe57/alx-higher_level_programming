465e0bc3e157a46babc23a13c6a0c47392dd0550
Techjoe-57 <117743341+Techjoe-57@users.noreply.github.com>
2023-04-12 14:48:41 +0000

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-
-Unittest for max_integer([..])
-
-"""
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_list_len(self):
-        # Test when len(list) == 0
+        # Test when list is empty
         self.assertFalse(max_integer([]))
+
+    def test_one_item(self):
+        # Test if list has only an item
+        self.assertAlmostEqual(max_integer([23]), 23)
 
     def test_list_values(self):
         # Test if value errors are raised
@@ -24,3 +24,15 @@ class TestMaxInteger(unittest.TestCase):
     def test_list_items(self):
         # Test if type errors are raised
         self.assertTrue(max_integer([1, 2, 3, False]), TypeError)
+
+    def test_negative_items(self):
+        # Test for maximum if all items are negative
+        self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_one_negative(self):
+        # Test for maximum if one negative number exists
+        self.assertAlmostEqual(max_integer([23, 0, -1]), 23)
+
+    def test_maximum_middle(self):
+        # Test for maximum if maximum item is in middle
+        self.assertAlmostEqual(max_integer([23, 100, -1]), 100)

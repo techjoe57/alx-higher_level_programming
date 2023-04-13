648ced4711c84d15af177854ff3db3a203e8eb33
Techjoe-57 <techjoe57@gmail.com>
2023-04-13 12:48:30 +0000

    Add class definitions

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Python module: Create a class with
+definitions
+"""
+class Rectangle:
+    """Defines the class Rectangle"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        """The width property"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """The width property setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height property"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """The property height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

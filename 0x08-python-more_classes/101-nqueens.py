a721663c3439aade2cea7c15600e264d24bac2a5
Techjoe-57 <117743341+Techjoe-57@users.noreply.github.com>
2023-04-14 15:57:57 +0000

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -114,14 +114,15 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-        if sys.argv[1].isdigit() is False:
-            print("N must be a number")
-            sys.exit(1)
-            if int(sys.argv[1]) < 4:
-                print("N must be at least 4")
-                sys.exit(1)
 
-                board = init_board(int(sys.argv[1]))
-                solutions = recursive_solve(board, 0, 0, [])
-                for sol in solutions:
-                    print(sol)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)

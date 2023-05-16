d6ad4183069fc9f96c64136841959a2267c1891d
Techjoe-57 <techjoe57@gmail.com>
2023-05-16 15:56:11 -0700

    Fix errors

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -13,7 +13,7 @@ COLLATE utf8mb4_unicode_ci;
 -- Convert Field name in first_table
 USE hbtn_0c_0;
 ALTER TABLE first_table
+CHANGE id id INT(11),
 CHANGE name name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-

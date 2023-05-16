377f4939cc66030960d311cf2d1e5a1f24d95aab
Techjoe-57 <117743341+Techjoe-57@users.noreply.github.com>
2023-05-16 22:15:40 +0000

    Fix alias

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Displays the average temperature (Fahrenheit) by 
 -- city ordered by temperature (descending)
-SELECT city, AVG(value) avg_tmp FROM temperatures GROUP BY city ORDER BY avg_tmp DESC;
+SELECT city, AVG(value) avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;

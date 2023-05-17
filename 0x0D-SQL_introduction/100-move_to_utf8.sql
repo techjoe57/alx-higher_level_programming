231d7b7060fe1ffdcd7084779d0913b8db8a946a
Techjoe-57 <117743341+Techjoe-57@users.noreply.github.com>
2023-05-17 09:20:14 +0000

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,7 +7,7 @@ COLLATE = utf8mb4_unicode_ci;
 -- Convert table first_table
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Convert Field name in first_table

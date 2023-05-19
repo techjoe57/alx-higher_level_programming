851dc9d112b18cc80cd74bdbeb530a4c08581fcc
Techjoe-57 <techjoe57@gmail.com>
2023-05-19 06:13:48 -0700

    Create unique column

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- create the table unique_id
+-- id description: id INT with default value 1 and must be unique
+-- name: VARCHAR(256)
+CREATE TABLE IF NOT EXISTS unique_id ( id INT DEFAULT '1', name VARCHAR(256), UNIQUE (id));

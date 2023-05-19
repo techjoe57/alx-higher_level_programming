b7e3069b80cbc7628d24659a20f253a9cadad8c5
Techjoe-57 <techjoe57@gmail.com>
2023-05-19 08:37:55 -0700

    Change sql keywords

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- list all cities contained in the database hbtn_0d_usa
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
-select cities.id, cities.name, states.name from cities inner join states on cities.state_id=states.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id;

2c0c1aff5c3b5e1fd062989712a91c69468d1fce
Techjoe-57 <117743341+Techjoe-57@users.noreply.github.com>
2023-05-22 13:32:34 +0000

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,5 +5,5 @@ SELECT tv_genres.name
 FROM tv_show_genres 
 RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id = 8
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name; 

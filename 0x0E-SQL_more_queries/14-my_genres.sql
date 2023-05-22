bc1e234d03445e0fa06d2999b38fb3f3195358d7
Techjoe-57 <techjoe57@gmail.com>
2023-05-22 06:29:40 -0700

    Fix join statements

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,8 @@
 -- each record should display: tv_genres.name
 -- results must be sorted in ascending order by the genre name
 SELECT tv_genres.name 
-FROM tv_show_genres RIGHT JOIN tv_genres 
-ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_show_genres 
+RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id = 8
-GROUP BY tv_genres.name
 ORDER BY tv_genres.name; 

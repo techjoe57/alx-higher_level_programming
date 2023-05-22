91242f1f41b7492b2d9f12f1e8858cef77d65b40
Techjoe-57 <techjoe57@gmail.com>
2023-05-22 06:25:22 -0700

    Fix errors

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,6 @@
 SELECT tv_genres.name 
 FROM tv_show_genres RIGHT JOIN tv_genres 
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id = 8; 
+WHERE tv_show_genres.show_id = 8
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name; 

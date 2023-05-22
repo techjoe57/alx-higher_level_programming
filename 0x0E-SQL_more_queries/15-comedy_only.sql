a61f37b0a75de88133f89c168836d776a70d5c3a
Techjoe-57 <techjoe57@gmail.com>
2023-05-22 06:43:41 -0700

    List comdey shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- list all Comedy shows in the database hbtn_0d_tvshows
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+SELECT tv_shows.title
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title;

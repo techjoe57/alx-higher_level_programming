e7ecf4a7d29d22ab6585e3a865e0f49993a4c9e1
Techjoe-57 <techjoe57@gmail.com>
2023-05-22 06:21:49 -0700

    List show genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- list all genres of the show Dexter
+-- each record should display: tv_genres.name
+-- results must be sorted in ascending order by the genre name
+SELECT tv_genres.name 
+FROM tv_show_genres RIGHT JOIN tv_genres 
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.show_id = 8; 

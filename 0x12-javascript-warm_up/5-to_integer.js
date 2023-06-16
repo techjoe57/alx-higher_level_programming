43dd00c8637b07314d3c7b7e2fb04a89867b80fb
Techjoe-57 <techjoe57@gmail.com>
2023-06-16 04:37:16 -0700

    Fix indent errors

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArg = Number(process.argv[2]);
 if (isNaN(firstArg)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number:', firstArg | 0);
+  console.log('My number:', firstArg | 0);
 }

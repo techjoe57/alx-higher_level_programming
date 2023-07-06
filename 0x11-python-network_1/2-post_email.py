39da36068b5083bab9a0a939f7e498ab0e2b736f
Techjoe-57 <techjoe57@gmail.com>
2023-07-06 03:47:53 -0700

    Create script to display variable value in response header

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-        url = sys.argv[1]
-        value = {"email": sys.argv[2]}
-        data = urllib.parse.urlencode(value).encode("ascii")
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-        request = urllib.request.Request(url, data)
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("utf-8"))
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
